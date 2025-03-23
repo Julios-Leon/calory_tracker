e28ee96982b5c3d621a75f96eeeb6179536a0693
Julios Leon <juliosmikel@gmail.com>
2025-03-23 11:52:04 +0100

    146

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -18,7 +18,10 @@ export const activityReducer = (
 
     if(action.type === "save-activity") {
         // Logica para actualizar el state
-        console.log("Desde el type de save-activity");
+        return {
+            ...state,
+            activities: [...state.activities, action.payload.newActivity]
+        }
     }
 
     return state
